7d866e5af7faab9e89ae2fe717c8780cb84f6b2f
PhuND <dinhphu951998@gmail.com>
2020-02-07 07:53:09 +0700

    init attendee project

---
+++ AttendanceSystemIPCamera/BackgroundServices/WindowAppRunnerService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AttendanceSystemIPCamera.BackgroundServices
+{
+    public class WindowAppRunnerService : BackgroundService
+    {
+        public WindowAppRunnerService()
+        {
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Thread myThread = new Thread(() =>
+            {
+                var application = new AttendeeWPF.App();
+                application.Run(new AttendeeWPF.MainWindow());  // add Window if you want a window.
+            });
+            myThread.SetApartmentState(ApartmentState.STA);
+            myThread.Start();
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ AttendanceSystemIPCamera/Test.cs
@@ -0,0 +1,25 @@
+﻿using AttendanceSystemIPCamera.Services.NetworkService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttendanceSystemIPCamera
+{
+    public class Test
+    {
+        public static void main()
+        {
+            //SupervisorNetworkService s = new SupervisorNetworkService();
+            //Thread tServer = new Thread(new ThreadStart(s.Start));
+            //tServer.Start();
+            //AttendeeNetworkService a = new AttendeeNetworkService();
+            //a.Start();
+
+            var service = new AttendeeNetworkService();
+            service.Start();
+
+            Console.Read();
+        }
+    }
+}
