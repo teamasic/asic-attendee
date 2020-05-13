b57ccd9da76cbad954787d7c9680db9ea18bc574
PhuND <dinhphu951998@gmail.com>
2020-05-13 09:48:02 +0700

    add support tool for publising the project

--- AttendeeApp/MainWindow.xaml.cs
+++ AttendeeApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AttendeeApp.Handler;
+using AttendeeApp.Utils;
 using CefSharp;
 using CefSharp.Wpf;
 using System;
@@ -24,25 +25,30 @@ namespace AttendeeWPF
     public partial class MainWindow : Window
     {
         private ChromiumWebBrowser browser;
-        private string initUrl = "https://localhost:44369/";
+        private Config config;
+        private const string CACHE_PATH = "BrowserCache";
 
         public MainWindow()
         {
             InitializeComponent();
+            config = Utils.GetConfig();
+
             InitBrowser();
         }
 
         private void InitBrowser()
         {
             CefSettings settings = new CefSettings();
-            settings.CachePath = "BrowserCache";
+            settings.CachePath = System.IO.Path.GetFullPath(CACHE_PATH);
 
             Cef.Initialize(settings);
 
-            browser = new ChromiumWebBrowser(initUrl);
+            browser = new ChromiumWebBrowser(config.Url);
             browser.RequestHandler = new CustomRequestHandler();
 
             grid.Children.Add(browser);
         }
+
     }
+
 }
